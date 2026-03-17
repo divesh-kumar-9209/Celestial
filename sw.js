56ccc9104acab79b10812342e7bded093406fee1
Divesh Kumar <diveshkumar9209@outlook.com>
2026-03-17 19:16:58 +0530

    app

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "celestial-v1";
+const urlsToCache = [
+  "/Celestial/",
+  "/Celestial/index.html",
+  "/Celestial/style.css",
+  "/Celestial/script.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+}); 
